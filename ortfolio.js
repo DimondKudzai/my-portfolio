7cd750408b7fc80cfcbd63bed61f4e8123b82087
DimondKudzai <48315329+DimondKudzai@users.noreply.github.com>
2019-03-14 05:11:49 -0700

    Add files via upload

---
+++ ortfolio.js
@@ -0,0 +1,14 @@
+var greet = new Date()
+var hr = greet.getHours()
+if(hr < 12){
+alert("good morning")
+}
+  else if(hr == 12){
+alert("good afternoon")
+}
+  else if(hr == 13){
+alert("good afternoon")
+}
+else{
+  alert("gud evining")
+}
