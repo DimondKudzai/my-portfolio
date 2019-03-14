96f2f0084500e076e482b3b3da1d9ba8303797da
DimondKudzai <48315329+DimondKudzai@users.noreply.github.com>
2019-03-14 07:04:46 -0700

    Add files via upload

---
+++ oortfolio.js
@@ -0,0 +1,23 @@
+var greet = new Date()
+var hr = greet.getHours()
+if(hr < 12){
+alert("Hello,good morning")
+}
+  else if(hr == 12){
+alert("Hello good afternoon")
+}
+  else if(hr == 13){
+alert("Hello,good afternoon")
+}
+  else if(hr == 14){
+alert("Hello,good afternoon")
+}
+  else if(hr == 15){
+alert("Hello,good afternoon")
+}
+  else if(hr == 16){
+alert("Hello,good afternoon")
+}
+else{
+  alert("Hello,good evining")
+}
